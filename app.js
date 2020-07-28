fe4f304e9b459254d88a59a0c28b3901920ac6e8
Zubs <zubairidrisaweda@gmail.com>
2020-07-28 20:45:30 +0100

    Started rendering with express

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+// Import express
 const express = require('express')
 
+// Initiate the app
 const app = express();
+
+// Listen to requests
+app.listen(3000);
+
+// Respond to /
+app.get('/', (req, res) => {
+	res.sendFile('./views/index.html', { root: __dirname })
+})
+
+// Respond to /
+app.get('/about', (req, res) => {
+	res.sendFile('./views/about.html', { root: __dirname })
+})
+
+app.use((req, res) => {
+	res.status(404).sendFile('./views/404.html', { root: __dirname })
+})
