bdece21672d4c250d778eb6f5e4b259f31b4d5bf
Zubs <zubairidrisaweda@gmail.com>
2020-07-29 21:13:26 +0100

    Set up ejs as my templating engine

--- app.js
+++ app.js
@@ -4,19 +4,22 @@ const express = require('express')
 // Initiate the app
 const app = express();
 
+// Register view engine
+app.set('view engine', 'ejs');
+
 // Listen to requests
 app.listen(3000);
 
 // Respond to /
 app.get('/', (req, res) => {
-	res.sendFile('./views/index.html', { root: __dirname })
-})
+	res.sendFile('./views/index.html', { root: __dirname });
+});
 
 // Respond to /
 app.get('/about', (req, res) => {
-	res.sendFile('./views/about.html', { root: __dirname })
-})
+	res.sendFile('./views/about.html', { root: __dirname });
+});
 
 app.use((req, res) => {
-	res.status(404).sendFile('./views/404.html', { root: __dirname })
-})
+	res.status(404).sendFile('./views/404.html', { root: __dirname });
+});
